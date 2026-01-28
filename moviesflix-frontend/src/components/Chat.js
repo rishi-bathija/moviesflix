24b527801c672081c20b42f0fa98beeae517db2c
Rishi Bathija <bathijarishi@gmail.com>
2026-01-28 20:18:54 +0530

    added backend urls

--- moviesflix-frontend/src/components/Chat.js
+++ moviesflix-frontend/src/components/Chat.js
@@ -58,7 +58,7 @@ const Chat = () => {
                 // Prepare the request body
                 const requestBody = JSON.stringify({ question: finalQuestion });
 
-                const response = await fetch("http://localhost:4000/api/user/generate-chat-response", {
+                const response = await fetch("https://moviesflix-xi.vercel.app/api/user/generate-chat-response", {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
